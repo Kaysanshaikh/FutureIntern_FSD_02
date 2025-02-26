64d2494f6e33e15f4c09ea4e9dfb31277ed8928e
Kaysan Shaikh <82015560+Kaysanshaikh@users.noreply.github.com>
2025-02-26 18:08:39 +0530

    Update axiosServices.js

--- client/src/axiosServices.js
+++ client/src/axiosServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = `http://localhost:3100`
+const baseURL = `https://future-intern-fsd-02.vercel.app/`
 
 export const axiosGet = (url) =>{
     return axios.get(`${baseURL}${url}`, {
